cbe83b681e03ba1f8e7470e19f5a064cf67e154b
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2023-12-07 23:41:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:8080"
+                url: "https://nodejs-job-portal-ymd1.onrender.com"
                 // url: "https://nodejs-job-portal-app.onrender.com"
             },
         ],
